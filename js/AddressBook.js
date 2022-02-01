c8ade7e1109350480b3923999bcb55d7ace92abb
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-02-01 08:43:16 +0530

    [neeta mourya] Add Form Input Fields to the Address Book Form UI

---
+++ js/AddressBook.js
@@ -0,0 +1,44 @@
+class EmployeePayrollData {
+
+
+    //getter and setter method
+    get fullName() {
+        return this._fullName;
+    }
+    set fullName(fullName) {
+        let nameRegex = RegExp('^[A-z]{1}[a-z]{2,}$');
+        if (nameRegex.test(fullName))
+            this.fullName = fullName;
+        else throw 'Name is incorrect';
+    }
+    get address() {
+        return this._address;
+    }
+    set address(address) {
+        this._address = address;
+    }
+    get city() {
+        return this._city;
+    }
+    set city(city) {
+        this._city = city;
+    }
+    get state() {
+        return this._state;
+    }
+    set state(state) {
+        this._state = state;
+    }
+    get zipCode() {
+        return this._zipCode;
+    }
+    set zipCode(zipCode) {
+        this._zipCode = zipCode;
+    }
+    get phoneNumber() {
+        return this._phoneNumber;
+    }
+    set phoneNumber(phoneNumber) {
+        this._phoneNumber = phoneNumber;
+    }
+}
